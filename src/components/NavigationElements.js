3edf8b07fdf84d5f8d9332f0cf256ee3a8b4fb5c
Kevin Mallari <kevinmallari62@gmail.com>
2021-08-25 17:10:02 +0800

    Added mobile menu.

--- src/components/NavigationElements.js
+++ src/components/NavigationElements.js
@@ -1,41 +1,60 @@
 import styled from "styled-components";
-import { NavLink as Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 import { FaBars } from "react-icons/fa";
 
 export const Nav = styled.nav`
-  background: #000;
+  position: sticky;
+  top: 0;
   height: 80px;
   display: flex;
   justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1000px) / 2);
+  padding: 0rem calc((100vw - 1440px) / 2);
   z-index: 999;
+  margin: 0;
+  background-color: white;
 `;
 
 export const NavLink = styled(Link)`
-  color: #fff;
+  color: #232323;
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 0 1rem;
+  padding: ${(props) => (!props.primary ? "0 1.2rem" : "0")};
   height: 100%;
   cursor: pointer;
+  font-size: 0.9rem;
+  transition: all 0.2s ease-in-out;
 
-  &.active {
-    color: #15cdfc;
+  &:hover {
+    // change colors of the next two lines
+    color: ${(props) => (!props.primary ? "blue" : "")};
+    background-color: ${(props) => (!props.primary ? "gray" : "")};
+    transition: all 0.2s ease-in-out;
+  }
+
+  @media screen and (max-width: 768px) {
+    justify-content: center;
+    width: 100%;
+
+    &:hover {
+      // change color
+      background-color: gray;
+      color: white;
+    }
   }
 `;
 
 export const Bars = styled(FaBars)`
   display: none;
-  color: #fff;
+  color: #232323;
 
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-    font-size: 1.8rem;
+    top: 27px;
+    right: 1rem;
+    /* transform: translate(-100%, 75%); */
+    font-size: 1.6rem;
     cursor: pointer;
   }
 `;
@@ -43,20 +62,37 @@ export const Bars = styled(FaBars)`
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
-  // margin-right: -24px;
+  margin-right: -1rem;
 
   // 2nd Nav
-  margin-right: 24px;
-
+  /* margin-right: 24px; */
   @media screen and (max-width: 768px) {
     display: none;
+  }
+`;
+
+export const MobileNavMenu = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    // change color
+    background-color: gray;
+    height: 600%;
+    position: absolute;
+    top: 80px;
+    left: ${(props) => props.left};
+    transition: all 0.2s ease-in-out;
   }
 `;
 
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
-  margin-right: 24px;
+  margin-right: 1rem;
 
   @media screen and (max-width: 768px) {
     display: none;
@@ -64,10 +100,11 @@ export const NavBtn = styled.nav`
 `;
 
 export const NavBtnLink = styled(Link)`
-  border-radius: 4px;
-  background: #256ce1;
-  padding: 10px; 22px;
-  color: #FFF;
+  border-radius: 99rem;
+  // change color
+  background: blue;
+  padding: 10px 22px;
+  color: #fff;
   border: none;
   outline: none;
   cursor: pointer;
@@ -79,7 +116,8 @@ export const NavBtnLink = styled(Link)`
 
   &:hover {
     transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #010606;
+    // change colors
+    background: gray;
+    color: black;
   }
 `;
--- src/components/index.js
+++ src/components/index.js
@@ -1,44 +1,85 @@
 import {
   Nav,
   NavLink,
-  Bars,
+  // Bars,
   NavMenu,
+  MobileNavMenu,
   NavBtn,
   NavBtnLink,
 } from "./NavigationElements";
+import "./Navigation.css";
+import { useState } from "react";
+import { HiMenuAlt3, HiOutlineX } from "react-icons/hi";
+import styled from "styled-components";
 
 const Navigation = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  const Bars = styled(menuClicked ? HiOutlineX : HiMenuAlt3)`
+    display: none;
+    color: #232323;
+
+    @media screen and (max-width: 768px) {
+      display: block;
+      position: absolute;
+      top: 27px;
+      right: 1rem;
+      /* transform: translate(-100%, 75%); */
+      font-size: 1.6rem;
+      cursor: pointer;
+    }
+  `;
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleMenuClick = () => {
+    console.log(menuClicked);
+    setMenuClicked(!menuClicked);
+  };
+
   return (
-    <>
-      <Nav>
-        <NavLink to="/">
-          <h1>LOGO</h1>
+    <Nav>
+      <div onClick={scrollToTop} className="logo">
+        <h1>LOGO</h1>
+      </div>
+      <NavMenu>
+        <NavLink smooth to="/#features">
+          Features
         </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/about" activeStyle>
-            About
-          </NavLink>
-          <NavLink to="/services" activeStyle>
-            Services
-          </NavLink>
-          <NavLink to="/contact-us" activeStyle>
-            Contact Us
-          </NavLink>
-          <NavLink to="/sign-up" activeStyle>
-            Sign Up
-          </NavLink>
-          <NavBtn>
-            <NavBtnLink to="/signin">Sign In</NavBtnLink>
-          </NavBtn>
-        </NavMenu>
-        {/* Uncomment code below and comment out the 
-        NavBtnLink in the NavMenu to make NavMenu center */}
-        {/* <NavBtn>
-          <NavBtnLink to="/signin">Sign In</NavBtnLink>
-        </NavBtn> */}
-      </Nav>
-    </>
+        <NavLink smooth to="/#about-us">
+          About Us
+        </NavLink>
+        <NavLink smooth to="/#products">
+          Products
+        </NavLink>
+        <NavLink smooth to="/#reviews">
+          Reviews
+        </NavLink>
+      </NavMenu>
+      <MobileNavMenu left={menuClicked ? "0" : "-100%"}>
+        <NavLink smooth to="/#features">
+          Features
+        </NavLink>
+        <NavLink smooth to="/#about-us">
+          About Us
+        </NavLink>
+        <NavLink smooth to="/#products">
+          Products
+        </NavLink>
+        <NavLink smooth to="/#reviews">
+          Reviews
+        </NavLink>
+      </MobileNavMenu>
+      <Bars onClick={handleMenuClick} menuClicked={menuClicked} />
+      <NavBtn>
+        <NavBtnLink to="/#footer">Contact Us</NavBtnLink>
+      </NavBtn>
+    </Nav>
   );
 };
 export default Navigation;
